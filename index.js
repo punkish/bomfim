ce37fb28f6401c8dfe5f7d4e7fca584b45770a8a
punkish <punk.kish@gmail.com>
2019-05-22 15:47:59 -0300

    added support for tags and tags-attribs reports

--- index.js
+++ index.js
@@ -8,17 +8,20 @@ const progress = require('progress');
 
 const xmldir = '/Users/punkish/Projects/zenodeo/data/treatmentsDump';
 const xmlfiles = fs.readdirSync(xmldir);
-let uniq = {};
+let uniqTags = {};
+let uniqTagsAttribs = {};
 
 const t0 = performance.now();
 
 let i = 0;
-const j = xmlfiles.length;
+let j = xmlfiles.length;
 
-const bar = new progress('  processing [:bar] :rate fps :percent :etas', {
+// update the progress bar very 100th of the total num of files
+const tickInterval = Math.floor(j/100);
+const bar = new progress('  processing [:bar] :rate files/sec :percent :etas', {
     complete: '=',
     incomplete: ' ',
-    width: 20,
+    width: 30,
     total: j
 });
 
@@ -29,8 +32,9 @@ for (; i < j; i++) {
         xmlMode: true
     });
 
-    if (!(j % Math.floor(j/10))) {
-        bar.tick();
+    // update progress bar
+    if (!(i % tickInterval)) {
+        bar.tick(tickInterval);
     }
 
     $('*')
@@ -40,19 +44,52 @@ for (; i < j; i++) {
 
             // escape embedded ':' in tagnames
             if (e.name.indexOf(':') !== -1) e.name = e.name.replace(':', '\\:');
-            return e.name
+
+            const attribs = Object.keys(e.attribs).sort();
+
+            return [
+                
+                // only the tag
+                e.name, 
+
+                // tag with its attributes
+                e.name + '\t' + (attribs.length ? attribs.join(', ') : '-') 
+            ]
         })
         .get()
-        .forEach(e => { uniq[e] = uniq[e] ? uniq[e] + 1 : 1 });
+        .forEach((e, i) => {
+
+            if (i % 2) {
+
+                // tags-attributes
+                uniqTagsAttribs[ e ] = uniqTagsAttribs[ e ] ? uniqTagsAttribs[ e ] + 1 : 1;
+            }
+            else {
+
+                // only tags
+                uniqTags[ e ] = uniqTags[ e ] ? uniqTags[ e ] + 1 : 1;
+            }
+             
+        });
 }
 
-// print an alphabetically sorted list of tags
-const uniqfmt = [];
-Object.keys(uniq)
+// alphabetically sorted list of only tags
+let rep1 = 'tag\tfrequency\n';
+Object.keys(uniqTags)
     .sort()
-    .forEach(t => { uniqfmt.push({tag: t, freq: uniq[t]}) });
+    .forEach(t => { 
+        rep1 += `${t}\t${uniqTags[t]}\n` 
+    });
 
-console.table(uniqfmt);
+fs.writeFileSync('reports/tags.txt', rep1, 'utf8');
+
+// alphabetically sorted list of tags-attributes
+let rep2 = 'tag\tattribs\tfrequency\n';
+Object.keys(uniqTagsAttribs)
+    .sort()
+    .forEach(t => { rep2 += `${t}\t${uniqTagsAttribs[t]}\n` });
+
+fs.writeFileSync('reports/tags-attributes.txt', rep2, 'utf8');
 
 const t1 = performance.now();
 console.log(`extracted unique tags from ${j} files in ${(t1 - t0).toFixed(2)} ms`);
